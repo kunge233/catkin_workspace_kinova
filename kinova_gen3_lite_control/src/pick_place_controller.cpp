bde39163fe572c0537e87ea2bed44996cb70591d
kunge233 <wbgz1103@163.com>
2024-07-29 21:27:00 +0800

    finish pick and place function in gazebo

--- kinova_gen3_lite_control/src/pick_place_controller.cpp
+++ kinova_gen3_lite_control/src/pick_place_controller.cpp
@@ -1,6 +1,9 @@
 #include <ros/ros.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 #include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit_msgs/GetPlanningScene.h>
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/CollisionObject.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 #include "ros/ros.h"
@@ -9,121 +12,51 @@
 #include "std_msgs/Empty.h"  
 #include <thread>
 #include <atomic>
-#include "kinova_gen3_lite_control/TargetPose.h"
-#include "kinova_gen3_lite_control/GripperValue.h"
-#include "kinova_gen3_lite_control/ActionTrigger.h"
-
-#include <kortex_driver/Base_ClearFaults.h>
-#include <kortex_driver/CartesianSpeed.h>
-#include <kortex_driver/BaseCyclic_Feedback.h>
-#include <kortex_driver/ReadAction.h>
-#include <kortex_driver/ExecuteAction.h>
-#include <kortex_driver/SetCartesianReferenceFrame.h>
-#include <kortex_driver/CartesianReferenceFrame.h>
-#include <kortex_driver/SendGripperCommand.h>
-#include <kortex_driver/GripperMode.h>
-#include <kortex_driver/ActionNotification.h>
-#include <kortex_driver/ActionEvent.h>
-#include <kortex_driver/ActionType.h>
-#include <kortex_driver/GetMeasuredCartesianPose.h>
-#include <kortex_driver/OnNotificationActionTopic.h>
 
 // The circle constant tau = 2*pi. One tau is one rotation in radians.
 const double tau = 2 * M_PI;
 
-bool sendGripperCommand(ros::NodeHandle n, double value) {
-    // 夹爪控制逻辑
-    // Initialize the ServiceClient
-    ros::ServiceClient service_client_send_gripper_command = n.serviceClient<kortex_driver::SendGripperCommand>("/my_gen3_lite/base/send_gripper_command");
-    kortex_driver::SendGripperCommand service_send_gripper_command;
-
-    // 限制value的值在0到1之间
-    if (value < 0.0) {
-        value = 0.0;
-    } else if (value > 1.0) {
-        value = 1.0;
-    }
-
-    // Initialize the request
-    kortex_driver::Finger finger;
-    finger.finger_identifier = 0; // 根据需要设置手指标识符
-    finger.value = 1.0 - value; // 设置夹爪开合程度
-    service_send_gripper_command.request.input.gripper.finger.push_back(finger);
-    service_send_gripper_command.request.input.mode = kortex_driver::GripperMode::GRIPPER_POSITION;
-
-    // 打印夹爪将要到达的闭合程度
-    ROS_INFO("Setting gripper to value of close: %f", finger.value);
-
-    if (service_client_send_gripper_command.call(service_send_gripper_command)) {
-        ROS_INFO("The gripper command was sent successfully.");
-
-    } else {
-        std::string error_string = "Failed to call service for sending gripper command";
-        ROS_ERROR("%s", error_string.c_str());
-        return false;
-    }
-    std::this_thread::sleep_for(std::chrono::milliseconds(50));
-    return true;
-}
-
 void openGripper(trajectory_msgs::JointTrajectory& posture)
 {
-    // BEGIN_SUB_TUTORIAL open_gripper
-    /* Add both finger joints of kinova robot. */
-    posture.joint_names.resize(2);
-    //posture.joint_names[0] = "left_finger_bottom_joint";
+    ROS_INFO("Opening gripper...");
+
+    posture.joint_names.resize(1);
     posture.joint_names[0] = "right_finger_bottom_joint";
 
-    /* Set them as open, wide enough for the object to fit. */
     posture.points.resize(1);
-    posture.points[0].positions.resize(2);
-    //posture.points[0].positions[0] = 0.0;
+    posture.points[0].positions.resize(1);
     posture.points[0].positions[0] = 0.96;
     posture.points[0].time_from_start = ros::Duration(0.5);
-    // END_SUB_TUTORIAL
 }
 
 void closedGripper(trajectory_msgs::JointTrajectory& posture)
 {
-    // BEGIN_SUB_TUTORIAL closed_gripper
-    /* Add both finger joints of kinova robot. */
-    posture.joint_names.resize(2);
-    //posture.joint_names[0] = "left_finger_bottom_joint";
+    ROS_INFO("Closing gripper...");
+
+    posture.joint_names.resize(1);
     posture.joint_names[0] = "right_finger_bottom_joint";
 
-    /* Set them as closed. */
     posture.points.resize(1);
-    posture.points[0].positions.resize(2);
-    posture.points[0].positions[0] = 0.00;
-    //posture.points[0].positions[1] = 0.00;
+    posture.points[0].positions.resize(1);
+    posture.points[0].positions[0] = -0.09;
     posture.points[0].time_from_start = ros::Duration(0.5);
-    // END_SUB_TUTORIAL
 }
 
-void pick(moveit::planning_interface::MoveGroupInterface& arm_group, moveit::planning_interface::MoveGroupInterface& gripper_group)
+void pick(moveit::planning_interface::MoveGroupInterface& move_group)
 {
-    // BEGIN_SUB_TUTORIAL pick1
-    // Create a vector of grasps to be attempted, currently only creating single grasp.
-    // This is essentially useful when using a grasp generator to generate and test multiple grasps.
     std::vector<moveit_msgs::Grasp> grasps;
     grasps.resize(1);
 
     // 设置抓取姿态
-    // ++++++++++++++++++++++
-    // Make sure that when you set the grasp_pose, you are setting it to be the pose of the last link in
-    // your manipulator which in this case would be `"XXX"` You will have to compensate for the
-    // transform from `"XXX"` to the palm of the end effector.
     grasps[0].grasp_pose.header.frame_id = "base_link";
     tf2::Quaternion orientation;
-    orientation.setRPY(-tau / 4, -tau / 8, -tau / 4);
+    orientation.setRPY (-tau / 4, 0, -tau / 4);     //(-tau / 4, -tau / 8, -tau / 4);固定轴欧拉角
     grasps[0].grasp_pose.pose.orientation = tf2::toMsg(orientation);
-    grasps[0].grasp_pose.pose.position.x = 0.415;
+    grasps[0].grasp_pose.pose.position.x = 0.5; //0.415+0.10;
     grasps[0].grasp_pose.pose.position.y = 0;
     grasps[0].grasp_pose.pose.position.z = 0.5;
         
     // 设置预抓取接近方式
-    // ++++++++++++++++++++++++++
-    /* Defined with respect to frame_id */
     grasps[0].pre_grasp_approach.direction.header.frame_id = "base_link";
     /* Direction is set as positive x axis */
     grasps[0].pre_grasp_approach.direction.vector.x = 1.0;
@@ -131,8 +64,6 @@ void pick(moveit::planning_interface::MoveGroupInterface& arm_group, moveit::pla
     grasps[0].pre_grasp_approach.desired_distance = 0.115;
 
     // 设置抓取后的退回方式
-    // ++++++++++++++++++++++++++
-    /* Defined with respect to frame_id */
     grasps[0].post_grasp_retreat.direction.header.frame_id = "base_link";
     /* Direction is set as positive z axis */
     grasps[0].post_grasp_retreat.direction.vector.z = 1.0;
@@ -140,105 +71,87 @@ void pick(moveit::planning_interface::MoveGroupInterface& arm_group, moveit::pla
     grasps[0].post_grasp_retreat.desired_distance = 0.25;
 
     // 设置抓取前的夹爪姿态
-    // +++++++++++++++++++++++++++++++++++
     openGripper(grasps[0].pre_grasp_posture);
 
     // 设置抓取时的夹爪姿态
-    // Setting posture of eef during grasp
-    // +++++++++++++++++++++++++++++++++++
     closedGripper(grasps[0].grasp_posture);
 
     // 设置支持面为 table1
-    arm_group.setSupportSurfaceName("table1");
+    move_group.setSupportSurfaceName("table1");
     // 调用 pick 函数执行抓取
-    arm_group.pick("object", grasps);
+    move_group.pick("object", grasps);
 }
 
-void place(moveit::planning_interface::MoveGroupInterface& arm_group, moveit::planning_interface::MoveGroupInterface& gripper_group)
+void place(moveit::planning_interface::MoveGroupInterface& move_group)
 {
+    // 创建一个存储放置位置的向量，大小为1
     std::vector<moveit_msgs::PlaceLocation> place_location;
     place_location.resize(1);
 
-    // Setting place location pose
-    // +++++++++++++++++++++++++++
+    // 设置放置位置的姿态
     place_location[0].place_pose.header.frame_id = "base_link";
     tf2::Quaternion orientation;
-    orientation.setRPY(0, 0, tau / 4);  // A quarter turn about the z-axis
+    orientation.setRPY(0, 0, tau / 4);  // 绕Z轴旋转四分之一圈
     place_location[0].place_pose.pose.orientation = tf2::toMsg(orientation);
 
-    /* For place location, we set the value to the exact location of the center of the object. */
+    // 对于放置位置，将值设置为对象中心的精确位置
     place_location[0].place_pose.pose.position.x = 0;
     place_location[0].place_pose.pose.position.y = 0.5;
     place_location[0].place_pose.pose.position.z = 0.5;
 
-    // Setting pre-place approach
-    // ++++++++++++++++++++++++++
-    /* Defined with respect to frame_id */
+    // 设置放置前的接近姿态
     place_location[0].pre_place_approach.direction.header.frame_id = "base_link";
-    /* Direction is set as negative z axis */
+    // 方向设置为负Z轴方向
     place_location[0].pre_place_approach.direction.vector.z = -1.0;
     place_location[0].pre_place_approach.min_distance = 0.095;
     place_location[0].pre_place_approach.desired_distance = 0.115;
 
-    // Setting post-grasp retreat
-    // ++++++++++++++++++++++++++
-    /* Defined with respect to frame_id */
+    // 设置放置后的撤退姿态
     place_location[0].post_place_retreat.direction.header.frame_id = "base_link";
-    /* Direction is set as negative y axis */
+    // 方向设置为负Y轴方向
     place_location[0].post_place_retreat.direction.vector.y = -1.0;
     place_location[0].post_place_retreat.min_distance = 0.1;
     place_location[0].post_place_retreat.desired_distance = 0.25;
 
-    // Setting posture of eef after placing object
-    // +++++++++++++++++++++++++++++++++++++++++++
-    /* Similar to the pick case */
+    // 设置放置后的夹爪姿态为打开
     openGripper(place_location[0].post_place_posture);
 
-    // Set support surface as table2.
-    arm_group.setSupportSurfaceName("table2");
-    // Call place to place the object using the place locations given.
-    arm_group.place("object", place_location);
-    // END_SUB_TUTORIAL
+    // 设置支撑表面为table2
+    move_group.setSupportSurfaceName("table2");
+    // 调用place函数，将对象放置到指定的位置
+    move_group.place("object", place_location);
 }
 
 void addCollisionObjects(moveit::planning_interface::PlanningSceneInterface& planning_scene_interface)
 {
-  // BEGIN_SUB_TUTORIAL table1
-  //
-  // Creating Environment
-  // ^^^^^^^^^^^^^^^^^^^^
-  // Create vector to hold 3 collision objects.
+  // 创建环境
+  // 向量保存3个碰撞对象
   std::vector<moveit_msgs::CollisionObject> collision_objects;
   collision_objects.resize(3);
 
-  // Add the first table where the cube will originally be kept.
-  collision_objects[0].id = "table1";
-  collision_objects[0].header.frame_id = "base_link";
+  // 第1个桌子
+  collision_objects[0].id = "table1"; // ID
+  collision_objects[0].header.frame_id = "base_link"; // 碰撞对象参考坐标系
 
-  /* Define the primitive and its dimensions. */
-  collision_objects[0].primitives.resize(1);
-  collision_objects[0].primitives[0].type = collision_objects[0].primitives[0].BOX;
-  collision_objects[0].primitives[0].dimensions.resize(3);
-  collision_objects[0].primitives[0].dimensions[0] = 0.2;
-  collision_objects[0].primitives[0].dimensions[1] = 0.4;
-  collision_objects[0].primitives[0].dimensions[2] = 0.4;
+  collision_objects[0].primitives.resize(1); // 形状
+  collision_objects[0].primitives[0].type = collision_objects[0].primitives[0].BOX; // 盒子
+  collision_objects[0].primitives[0].dimensions.resize(3); // 定义形状的尺寸数组
+  collision_objects[0].primitives[0].dimensions[0] = 0.2; // 设置盒子的长
+  collision_objects[0].primitives[0].dimensions[1] = 0.4; // 设置盒子的宽
+  collision_objects[0].primitives[0].dimensions[2] = 0.4; // 设置盒子的高
 
-  /* Define the pose of the table. */
-  collision_objects[0].primitive_poses.resize(1);
+  collision_objects[0].primitive_poses.resize(1); // 姿态
   collision_objects[0].primitive_poses[0].position.x = 0.5;
   collision_objects[0].primitive_poses[0].position.y = 0;
   collision_objects[0].primitive_poses[0].position.z = 0.2;
-  collision_objects[0].primitive_poses[0].orientation.w = 1.0;
-  // END_SUB_TUTORIAL
+  collision_objects[0].primitive_poses[0].orientation.w = 1.0; // 无旋转
 
   collision_objects[0].operation = collision_objects[0].ADD;
 
-  // BEGIN_SUB_TUTORIAL table2
-  // Add the second table where we will be placing the cube.
+  // 第2个桌子
   collision_objects[1].id = "table2";
   collision_objects[1].header.frame_id = "base_link";
 
-  /* Define the primitive and its dimensions. */
   collision_objects[1].primitives.resize(1);
   collision_objects[1].primitives[0].type = collision_objects[1].primitives[0].BOX;
   collision_objects[1].primitives[0].dimensions.resize(3);
@@ -246,22 +159,19 @@ void addCollisionObjects(moveit::planning_interface::PlanningSceneInterface& pla
   collision_objects[1].primitives[0].dimensions[1] = 0.2;
   collision_objects[1].primitives[0].dimensions[2] = 0.4;
 
-  /* Define the pose of the table. */
+
   collision_objects[1].primitive_poses.resize(1);
   collision_objects[1].primitive_poses[0].position.x = 0;
   collision_objects[1].primitive_poses[0].position.y = 0.5;
   collision_objects[1].primitive_poses[0].position.z = 0.2;
   collision_objects[1].primitive_poses[0].orientation.w = 1.0;
-  // END_SUB_TUTORIAL
 
   collision_objects[1].operation = collision_objects[1].ADD;
 
-  // BEGIN_SUB_TUTORIAL object
-  // Define the object that we will be manipulating
+  // 操作对象
   collision_objects[2].header.frame_id = "base_link";
   collision_objects[2].id = "object";
 
-  /* Define the primitive and its dimensions. */
   collision_objects[2].primitives.resize(1);
   collision_objects[2].primitives[0].type = collision_objects[1].primitives[0].BOX;
   collision_objects[2].primitives[0].dimensions.resize(3);
@@ -269,16 +179,15 @@ void addCollisionObjects(moveit::planning_interface::PlanningSceneInterface& pla
   collision_objects[2].primitives[0].dimensions[1] = 0.02;
   collision_objects[2].primitives[0].dimensions[2] = 0.2;
 
-  /* Define the pose of the object. */
   collision_objects[2].primitive_poses.resize(1);
   collision_objects[2].primitive_poses[0].position.x = 0.5;
   collision_objects[2].primitive_poses[0].position.y = 0;
   collision_objects[2].primitive_poses[0].position.z = 0.5;
   collision_objects[2].primitive_poses[0].orientation.w = 1.0;
-  // END_SUB_TUTORIAL
 
   collision_objects[2].operation = collision_objects[2].ADD;
 
+  //应用到规划场景
   planning_scene_interface.applyCollisionObjects(collision_objects);
 }
 
@@ -297,14 +206,12 @@ int main(int argc, char** argv)
 
     addCollisionObjects(planning_scene_interface);
 
-    // Wait a bit for ROS things to initialize
-    ros::WallDuration(1.0).sleep();
+    // 创建 JointTrajectory 对象
+    trajectory_msgs::JointTrajectory gripper_posture;
 
-    pick(arm_group, gripper_group);
+    pick(arm_group);
 
-    ros::WallDuration(1.0).sleep();
-
-    place(arm_group, gripper_group);
+    place(arm_group);
 
     ros::waitForShutdown();
     return 0;
